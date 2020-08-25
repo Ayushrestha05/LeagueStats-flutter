4245c19a72c8ae91c4955d90a6ed33cfb863e948
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-08-25 19:55:44 +0545

    Created search_page.dart

---
+++ lib/Screens/search_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  static final String id = 'search_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
