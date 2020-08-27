e8bba949aa1b060f6b250601e26e7033fd7c481c
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-08-27 16:36:48 +0545

    Added Navigator to push the screen to summoner_details

--- lib/Screens/search_page.dart
+++ lib/Screens/search_page.dart
@@ -1,13 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:league_stats_flutter/Screens/summoner_details.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   static final String id = 'search_page';
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  var selectedRegion = 'NA';
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List regionList = [
+      'NA',
+      'EUN',
+      'EUW',
+      'JP',
+      'KR',
+      'LAN',
+      'LAS',
+      'BRA',
+      'OCE',
+      'TUR',
+      'RU',
+    ];
+    List<DropdownMenuItem<String>> dropdownItems = [];
+
+    for (int i = 0; i < regionList.length; i++) {
+      dropdownItems.add(
+        DropdownMenuItem(
+          child: Text(regionList[i]),
+          value: regionList[i],
+        ),
+      );
+    }
+
+    return dropdownItems;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      drawer: Drawer(),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.maxFinite,
+              height: 150,
+              child: Image(
+                image: AssetImage('resources/images/LOL_logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                DropdownButton<String>(
+                  value: selectedRegion,
+                  items: getDropdownItems(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedRegion = value;
+                      print(value);
+                    });
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    onSubmitted: (value) {
+                      print(value);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Summoner(),
+                        ),
+                      );
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Enter your Summoner\'s name',
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
