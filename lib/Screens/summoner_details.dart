251ed80f79e4cf1278c870b341e11b12ef333b38
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-08-28 13:42:48 +0545

    Changed from Stateless Widget to Stateful Widget

--- lib/Screens/summoner_details.dart
+++ lib/Screens/summoner_details.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:league_stats_flutter/Components/networking.dart';
 
-class Summoner extends StatelessWidget {
+class Summoner extends StatefulWidget {
+  final selectedRegion;
+  final summonerName;
+  Summoner({this.selectedRegion, this.summonerName});
+  @override
+  _SummonerState createState() => _SummonerState();
+}
+
+class _SummonerState extends State<Summoner> {
+  @override
+  void initState() {
+    super.initState();
+    Networking().getSummonerDetails(widget.selectedRegion, widget.summonerName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
