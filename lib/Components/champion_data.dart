c5796e989faf5a9ae5e54820bb5f11f2850ebc99
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-09-02 16:25:05 +0545

    Removed Un-necessary imports

--- lib/Components/champion_data.dart
+++ lib/Components/champion_data.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 
 class ChampionData {
--- lib/Screens/summoner_details.dart
+++ lib/Screens/summoner_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:league_stats_flutter/Components/champion_data.dart';
 import 'package:league_stats_flutter/Components/networking.dart';
 
 class Summoner extends StatefulWidget {
