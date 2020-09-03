df1260579cc2d00d5fde0adc2d73e9449853f669
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-09-03 21:10:55 +0545

    Added a class that receive Match Data

--- lib/Components/networking.dart
+++ lib/Components/networking.dart
@@ -1,12 +1,11 @@
 import 'package:http/http.dart' as http;
 
 class Networking {
-  static const api_key = null;
-  void getSummonerDetails(selectedRegion, summonerName) async {
-    print("$selectedRegion,$summonerName");
+  static const api_key = 'RGAPI-1cc97b13-c73f-41ae-845f-c35f3167a9d0';
+  Future getSummonerDetails(selectedRegion, summonerName) async {
     var jsonData = await http.get(
         'https://${getRegionHTTP(selectedRegion)}/lol/summoner/v4/summoners/by-name/$summonerName?api_key=$api_key');
-    print(jsonData.body);
+    return (jsonData.body);
   }
 
   String getRegionHTTP(selectedRegion) {
@@ -24,5 +23,12 @@ class Networking {
       'RU': 'ru.api.riotgames.com'
     };
     return regionHTTPS[selectedRegion];
+  }
+
+  Future<String> getMatchData(selectedRegion, accountID) async {
+    var matchData = await http.get(
+        'https://${getRegionHTTP(selectedRegion)}/lol/match/v4/matchlists/by-account/$accountID?api_key=$api_key');
+
+    return (matchData.body);
   }
 }
